3e849eecc70cb301d5fb6e861250d3e7dcb2b4d4
Jonnas Cipriano <jonnascipriano@gmail.com>
2026-03-04 22:34:57 -0300

    pacotes concluídos

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+
 
 
 module.exports = {
@@ -24,5 +26,34 @@ module.exports = {
     plugins: [new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "index.html"),
         favicon: path.resolve("src", "assets", "scissors.svg")
-    })]
+    }),
+
+    new CopyWebpackPlugin({
+        patterns: [
+            {
+                from: path.resolve(__dirname, "src", "assets"),
+                to: path.resolve(__dirname, "dist", "src", "assets")
+            }
+        ]
+    })
+],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    }
 }
