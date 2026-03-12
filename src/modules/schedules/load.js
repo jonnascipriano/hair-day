34f0c0539b033d5522f2927abda6151bcdde4e5d
Jonnas Cipriano <jonnascipriano@gmail.com>
2026-03-11 21:44:08 -0300

    att

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,11 +1,16 @@
+import { scheduleFetchByDay } from "../../services/schedules-fetch-by-day.js"
 import { hoursLoad } from "../form/hours.load.js"
 
 // Seleciona o input de data.
 const selectedDate = document.getElementById('date')
 
-export function schedulesDays(){
+export async function schedulesDays(){
     // Obtém a data do input
     const date = selectedDate.value
+
+    // Busca na API os agendamentos
+    const dailySchedules = await scheduleFetchByDay({date})
+    console.log(dailySchedules)
 
     // Busca na API os agendamentos para carregar do lado direito da tela.
     // Renderiza as horas disponíveis.
--- src/services/schedules-new.js
+++ src/services/schedules-new.js
@@ -2,6 +2,7 @@ import { apiConfig } from "./api-config.js";
 
 export async function scheduleNew({id, name, when}){
     try {
+        // Faz a requisiçao para enviar os dados do agendamento
         await fetch(`${apiConfig.baseURL}/schedules`, {
             method: "POST",
             headers: {
@@ -14,6 +15,7 @@ export async function scheduleNew({id, name, when}){
             })
         })
 
+        // Exibe mensagens de agendamento realizado.
         alert("Agendamento realizado com sucesso!")
     } catch (error) {
         console.log(error)
